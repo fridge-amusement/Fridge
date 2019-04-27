64f207c7fa0a7aa5c5c6301d79bbb6baba8fe56b
Kristi <kltakach@gmail.com>
2019-04-27 12:24:13 -0400

    file to show view of timesheet

---
+++ timesheet.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <div style="height:900px; background-color: lightblue;" align="center">
+        <br><br><br><br>
+
+
+        <?php
+        require("dbconnect.php");
+
+        require("functions/viewEmployee.php");
+
+        show_timesheet($conn);
+        ?>
+
+
+
+        <br><br><br><br>
+        <hr width="50">
+        <a href="Frontpage.html" style="color:red;font-weight:bold;">Home</a>
+        <hr width="50">
+    </div>
+</body>
+
+</html>
